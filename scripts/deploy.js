610e4770fe2455acb3c83c5ead520779270dc5d9
JoelDeTrinidad <joeldetrinidad@gmail.com>
2021-08-25 21:03:20 -0600

    Added ERC20 standard token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,11 +14,16 @@ async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
+  const JuegaCoinToken = await hre.ethers.getContractFactory("JuegaCoinToken");
+  const juegacoinToken = await JuegaCoinToken.deploy("Juega Coin", "JuegaCoin");
+
   await greeter.deployed();
   await token.deployed();
+  await juegacoinToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
+  console.log("juegacoinToken deployed to:", juegacoinToken.address);
 }
 
 main()
